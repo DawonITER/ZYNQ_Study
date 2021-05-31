280753320b06e29023daee6b215d4b23d574a6a7
Ggultea_Yslee <ggulteawiki@gmail.com>
2021-06-01 08:57:50 +0900

    6주차 교육자료 업로드

---
+++ Lab6/simulation/simulation.sdk/Helloworld/Debug/src/helloworld.d
@@ -0,0 +1,21 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../FSBL_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../FSBL_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../FSBL_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../FSBL_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../FSBL_bsp/ps7_cortexa9_0/include/bspconfig.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../FSBL_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../FSBL_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../FSBL_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../FSBL_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../FSBL_bsp/ps7_cortexa9_0/include/bspconfig.h:
